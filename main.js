def5c8d4c1a3ad0910867aed2c19369198930fd4
Fatih Karagöz <148713912+fmkaragoz@users.noreply.github.com>
2023-12-12 22:06:50 +0300

    Update main.js

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ const themeButton = document.querySelector("#theme-btn");
 const deleteButton = document.querySelector("#delete-btn");
 
 let userText = null;
-const API_KEY = "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";
+const API_KEY = "YOUR_API_KEY";
 const initialHeight = chatInput.scrollHeight;
 
 
